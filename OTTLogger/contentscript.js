2a67edb93687cddff29df474e7c9d1edb6658698
munhyunsu <munhyunsu@gmail.com>
2018-09-10 06:59:14 +0900

    Fix

--- OTTLogger/contentscript.js
+++ OTTLogger/contentscript.js
@@ -5,9 +5,9 @@ function getHTML(){
   return document.body.outerHTML;
 }
 //debugger;
-//console.log(document.title);
+console.log(document.title);
 //console.log(getText());             //Gives you all the text on the page
-//console.log(getHTML());             //Gives you the whole HTML of the page
+console.log(getHTML());             //Gives you the whole HTML of the page
 
 chrome.runtime.sendMessage(
     {
--- README.md
+++ README.md
@@ -6,3 +6,5 @@
 
 ## OTTLogger
 - OTT 시청 컨텐츠 저장 크롬 확장 프로그램
+
+## User behavior recognizing
